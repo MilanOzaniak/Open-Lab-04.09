f5cfddf02f2656d972df00b5d09257cfc6760416
Ozaniak <Ozaniak@PC>
2020-10-22 21:46:13 +0200

    hotovo

--- Open-Lab-04.09/ArrayTools.cs
+++ Open-Lab-04.09/ArrayTools.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Open_Lab_04._09
 {
@@ -6,7 +8,9 @@ namespace Open_Lab_04._09
     {
         public string[] RemoveDups(string[] strings)
         {
-            throw new NotImplementedException();
+            var list = new List<string>(strings);
+            return list.Distinct().ToArray();
+
         }
     }
 }
